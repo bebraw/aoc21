128094391914750601ef310171a9cc3e8ec4797f
Juho Vepsäläinen <bebraw@gmail.com>
2021-12-04 09:13:27 +0200

    chore: Sketch out bingo matching

--- 04.ts
+++ 04.ts
@@ -1,12 +1,15 @@
+type BingoSheet = number[][];
+
 const lines = (await Deno.readTextFile("./4-test-input.txt")).split("\n");
 const numbers = lines[0].split(",").map((n) => parseInt(n, 10));
 const bingoSheets = getBingoSheets(lines);
+const firstBingoMatch = findFirstBingoMatch(bingoSheets, numbers);
 
-console.log(numbers, bingoSheets);
+console.log(numbers, bingoSheets, firstBingoMatch);
 
 function getBingoSheets(lines: string[]) {
-  const ret: number[][][] = [];
-  let accumulator: number[][] = [];
+  const ret: BingoSheet[] = [];
+  let accumulator: BingoSheet = [];
 
   lines.slice(2).forEach((line) => {
     if (line.trim() === "") {
@@ -18,6 +21,52 @@ function getBingoSheets(lines: string[]) {
   });
 
   ret.push(accumulator);
+
+  return ret;
+}
+
+function findFirstBingoMatch(sheets: BingoSheet[], numbers: number[]) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < sheets.length; j++) {
+      if (checkBingoMatch(sheets[j], numbers.slice(0, i))) {
+        return sheets[j];
+      }
+    }
+  }
+}
+
+function checkBingoMatch(sheet: BingoSheet, numbers: number[]) {
+  const matches = sheet.map((row) => row.map((v) => numbers.includes(v)));
+
+  let hasRowMatch = false;
+  matches.forEach((row) => {
+    if (row.every((r) => r === true)) {
+      hasRowMatch = true;
+    }
+  });
+
+  let hasColumnMatch = false;
+  transpose(matches).forEach((column) => {
+    if (column.every((r) => r === true)) {
+      hasColumnMatch = true;
+    }
+  });
+
+  return hasRowMatch || hasColumnMatch;
+}
+
+function transpose(sheet: unknown[][]) {
+  const ret: unknown[][] = [];
+
+  sheet.forEach((row) => {
+    row.forEach((v, j) => {
+      if (ret.length < j + 1) {
+        ret.push([v]);
+      } else {
+        ret[j].push(v);
+      }
+    });
+  });
 
   return ret;
 }

e3582ddce36db4b89658f5262d4f035280cd0c61
Juho Vepsäläinen <bebraw@gmail.com>
2021-12-05 10:15:29 +0200

    chore: Add debug helper

--- 05.ts
+++ 05.ts
@@ -8,7 +8,7 @@ type Line = {
   to: Coordinate;
 };
 
-const lines: Line[] = (await Deno.readTextFile("./5-input.txt")).split(
+const lines: Line[] = (await Deno.readTextFile("./5-test-input.txt")).split(
   "\n",
 ).map(
   (s) => ({
@@ -25,7 +25,8 @@ const lines: Line[] = (await Deno.readTextFile("./5-input.txt")).split(
 const hits = calculateHits(lines);
 const points = calculatePoints(hits);
 
-console.log(points);
+console.log(printDiagram(hits));
+console.log({ points });
 
 function calculateHits(lines: Line[]): Record<string, number> {
   const hits: Record<string, number> = {};
@@ -59,6 +60,45 @@ function calculateHits(lines: Line[]): Record<string, number> {
             }
           },
         );
+    } else if (from.x === from.y && to.x === to.y) {
+      // TODO
+      console.log("diagonal match 1", from, to);
+
+      // TODO: Adjust this
+      Array.from(
+        new Array(Math.abs(to.x - from.x) + 1).keys(),
+        (v) => v + Math.min(to.x, from.x),
+      )
+        .forEach(
+          (x, i) => {
+            console.log(x, from.y + i);
+
+            if (hits[`${x}-${from.y + i}`]) {
+              hits[`${x}-${from.y + i}`]++;
+            } else {
+              hits[`${x}-${from.y + i}`] = 1;
+            }
+          },
+        );
+    } else if (from.x === to.y && from.y === to.x) {
+      // TODO
+      console.log("diagonal match 2", from, to);
+
+      Array.from(
+        new Array(Math.abs(to.x - from.x) + 1).keys(),
+        (v) => Math.max(to.x, from.x) - v,
+      )
+        .forEach(
+          (x, i) => {
+            console.log(x, from.y + i);
+
+            if (hits[`${x}-${from.y + i}`]) {
+              hits[`${x}-${from.y + i}`]++;
+            } else {
+              hits[`${x}-${from.y + i}`] = 1;
+            }
+          },
+        );
     }
   });
 
@@ -67,4 +107,22 @@ function calculateHits(lines: Line[]): Record<string, number> {
 
 function calculatePoints(hits: Record<string, number>) {
   return Object.values(hits).filter((v) => v > 1).length;
+}
+
+function printDiagram(hits: Record<string, number>) {
+  // Hardcoded to zero-indexed 9x9 for now
+  const coordinates = Array.from(
+    new Array(10).keys(),
+    () => Array.from(new Array(10).keys(), () => 0),
+  );
+
+  Object.entries(hits).forEach(([k, v]) => {
+    const [x, y] = k.split("-");
+
+    coordinates[parseInt(x, 10)][parseInt(y, 10)] = v;
+  });
+
+  return coordinates.map((row) => row.map((v) => v || ".").join("")).join(
+    "\n",
+  );
 }

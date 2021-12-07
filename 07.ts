e495a13efec486577b1dd11e975581fe838ad6a7
Juho Vepsäläinen <bebraw@gmail.com>
2021-12-07 09:17:14 +0200

    chore: Add part one for day 7

--- 07.ts
+++ 07.ts
@@ -1,6 +1,25 @@
-const locations: number[] = (await Deno.readTextFile("./7-test-input.txt"))
+const locations: number[] = (await Deno.readTextFile("./7-input.txt"))
   .split(
     ",",
   ).map((s) => parseInt(s, 10));
 
-console.log(locations);
+console.log(findTheCheapestMove(locations));
+
+function findTheCheapestMove(locations: number[]) {
+  const options = Array.from(new Array(Math.max.apply(null, locations)).keys());
+
+  return Math.min.apply(
+    null,
+    options.map((o) => getFuelSpentWhileMoving(locations, o)),
+  );
+}
+
+function getFuelSpentWhileMoving(locations: number[], position: number) {
+  let fuelSpent = 0;
+
+  locations.forEach((l) => {
+    fuelSpent += Math.abs(l - position);
+  });
+
+  return fuelSpent;
+}

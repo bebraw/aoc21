60f19915d5b4d4d6f9d940d108043b1135564afb
Juho Vepsäläinen <bebraw@gmail.com>
2021-12-08 11:47:50 +0200

    chore: Adjust solution

--- 08.ts
+++ 08.ts
@@ -2,13 +2,14 @@ type Character = Set<string>;
 type Segments = { begin: Character[]; end: Character[] };
 
 // TODO: Change alignment per column as that's where comparison can be done properly
-const segments: Segments[] = (await Deno.readTextFile("./8-test-input.txt"))
+const segments: Segments[] = (await Deno.readTextFile("./8-test-input-2.txt"))
   .split(
     "\n",
   ).map(parseSegment);
+const segmentsPerColumn = columnize(segments);
 
 // console.log(segments);
-console.log(evaluateAllSegments(segments));
+console.log(evaluateAllSegments(segmentsPerColumn));
 
 function parseSegment(s: string) {
   const [begin, end] = s.split("|");
@@ -19,16 +20,32 @@ function parseSegment(s: string) {
   };
 }
 
-function evaluateAllSegments(allSegments: Segments[]) {
-  allSegments.forEach(evaluateSegments);
+function columnize(segments: Segments[]) {
+  const ret: Character[][] = Array.from(new Array(14).keys(), () => []);
+
+  segments.forEach(({ begin, end }) => {
+    begin.forEach((s, i) => {
+      ret[i].push(s);
+    });
+    end.forEach((s, i) => {
+      ret[10 + i].push(s);
+    });
+  });
+
+  return ret;
 }
 
-function evaluateSegments(segments: Segments) {
+function evaluateAllSegments(segments: Character[][]) {
+  segments.forEach(evaluateSegments);
+}
+
+function evaluateSegments(segmentsPerColumn: Character[]) {
+  // TODO: Generalize this per display
   const finalSegments: Record<string, string> = {};
   const foundNumbers: Record<string, Character> = {};
   const sixOrNine: Character[] = [];
 
-  segments.begin.forEach((segment) => {
+  segmentsPerColumn.forEach((segment) => {
     if (hasOne(segment)) {
       foundNumbers[1] = segment;
     }
@@ -52,10 +69,25 @@ function evaluateSegments(segments: Segments) {
     finalSegments[top] = "a";
   }
 
+  if (foundNumbers[4] && foundNumbers[7]) {
+    console.log(
+      "got both 4 and 7",
+      segmentsPerColumn,
+      foundNumbers[7],
+      foundNumbers[4],
+      subtract(foundNumbers[7], foundNumbers[4]),
+    );
+  }
+
   // TODO: If 5 and 6 are found, then e can be figured out
   // TODO: If 8 and 9 are found, then e can be figured out
   // TODO: Check six and nine
-  console.log(foundNumbers, finalSegments, sixOrNine);
+  console.log(
+    "final segments",
+    finalSegments,
+    "six or nine",
+    sixOrNine,
+  );
 
   return "foobar";
 }
@@ -84,4 +116,8 @@ function difference<T>(a: Set<T>, b: Set<T>) {
   return [...a].filter((x) => !b.has(x)).concat(
     [...b].filter((x) => !a.has(x)),
   );
+}
+
+function subtract<T>(a: Set<T>, b: Set<T>) {
+  return [...a].filter((x) => !b.has(x));
 }
